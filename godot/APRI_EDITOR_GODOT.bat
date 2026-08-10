d7f1bc8ef41507b11dd956be432ab27200c77416
Palermo Streets <palermo-streets@local.invalid>
2026-08-10 14:41:53 +0200

    fix: launch Godot with compatible renderer

--- godot/APRI_EDITOR_GODOT.bat
+++ godot/APRI_EDITOR_GODOT.bat
@@ -1,9 +1,13 @@
 @echo off
-set "GODOT=%~dp0..\Tools\Godot-4.7.1\Godot_v4.7.1-stable_win64.exe"
+setlocal
+set "GODOT=C:\Users\Utente\Documents\PIXIES\Tools\Godot-4.7.1\Godot_v4.7.1-stable_win64.exe"
+if not exist "%GODOT%" set "GODOT=%~dp0..\Tools\Godot-4.7.1\Godot_v4.7.1-stable_win64.exe"
 if not exist "%GODOT%" set "GODOT=%~dp0..\..\Tools\Godot-4.7.1\Godot_v4.7.1-stable_win64.exe"
 if not exist "%GODOT%" (
-  echo Godot non trovato in: %GODOT%
+  echo ERRORE: Godot non trovato.
+  echo Cartella attesa: C:\Users\Utente\Documents\PIXIES\Tools\Godot-4.7.1
   pause
   exit /b 1
 )
-start "Palermo Streets Godot" "%GODOT%" --editor --path "%~dp0"
+start "" /D "%~dp0" "%GODOT%" --editor --path "%~dp0" --rendering-method gl_compatibility
+exit /b 0
